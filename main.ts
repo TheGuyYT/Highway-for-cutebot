7ccf6c832a41f03cc319817294be9c1e86efdda7
TheGuyYT <102736803+TheGuyYT@users.noreply.github.com>
2022-04-01 14:13:32 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,20 +2,21 @@ let Sonar = 0
 basic.forever(function () {
     Sonar = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
     if (Sonar > 2 && Sonar < 15) {
-        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # . .
-            . . . . .
-            . . # . .
-            `)
-        cuteBot.motors(-25, -25)
-        for (let index = 0; index < 4; index++) {
-            music.playTone(440, music.beat(BeatFraction.Quarter))
-        }
-    } else {
         cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ff00)
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+        for (let index = 0; index < 15; index++) {
+            music.playMelody("C5 B A G F E D C ", 1000)
+            music.playMelody("C D E F G A B C5 ", 1000)
+        }
+        cuteBot.stopcar()
+    } else {
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff8000)
         basic.showLeds(`
             . . # . .
             . # # # .
